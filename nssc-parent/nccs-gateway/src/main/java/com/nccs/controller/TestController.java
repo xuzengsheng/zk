0dbdbe06d5378a1d967b44314b95ffc31e325766
ex_xuzc <ex_xuzc@partner.midea.com>
2020-10-10 15:01:50 +0800

    新增订阅发布通知测试类，删除旧的测试模块

--- nssc-parent/nccs-gateway/src/main/java/com/nccs/controller/TestController.java
+++ nssc-parent/nccs-gateway/src/main/java/com/nccs/controller/TestController.java
@@ -1,8 +1,12 @@
 package com.nccs.controller;
 
 import com.nccs.custom.BaseResponse;
+import com.nccs.event.OrderEvent;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -13,11 +17,28 @@ import org.springframework.web.bind.annotation.RestController;
  **/
 @RestController
 public class TestController {
+    @Autowired
+    private ApplicationContext applicationContext;
+
+
     @Value("${server.port}")
     private int port;
 
     @GetMapping("/test")
     public BaseResponse test(){
         return BaseResponse.success(port);
+    }
+
+    /**
+     * 调用事件通知
+     *
+     * @param message
+     * @return
+     */
+    @GetMapping("/provide/order")
+    public String order(@RequestParam("message") String message) {
+        OrderEvent orderEvent = new OrderEvent(this, message);
+        applicationContext.publishEvent(orderEvent);
+        return message;
     }
 }
